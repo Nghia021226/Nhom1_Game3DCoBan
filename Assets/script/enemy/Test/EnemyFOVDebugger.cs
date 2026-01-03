7750a992e70f952d597e51173b970e0f3b6d4cec
CTCuong23 <phucuong10082003@gmail.com>
2026-01-03 16:37:15 +0700

    Add a vision function for monsters.
    cho quái không nhìn xuyên tường

---
+++ Assets/script/enemy/Test/EnemyFOVDebugger.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class EnemyFOVDebugger : MonoBehaviour
+{
+    [SerializeField] float viewDistance = 10f;
+    [SerializeField] float viewAngle = 90f;
+    [SerializeField] Color coneColor = new Color(0, 1, 0, 0.2f); // Màu xanh lá mờ
+
+    // Hàm này sẽ vẽ hình lên cửa sổ Scene
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = coneColor;
+        Vector3 pos = transform.position + Vector3.up; // Vẽ từ ngang ngực
+
+        // Vẽ 2 đường biên của hình nón
+        Vector3 leftDir = Quaternion.AngleAxis(-viewAngle * 0.5f, Vector3.up) * transform.forward;
+        Vector3 rightDir = Quaternion.AngleAxis(viewAngle * 0.5f, Vector3.up) * transform.forward;
+
+        Gizmos.DrawLine(pos, pos + leftDir * viewDistance);
+        Gizmos.DrawLine(pos, pos + rightDir * viewDistance);
+
+        // Vẽ một vòng cung nhỏ để tạo thành hình nón (Optional)
+        int segments = 10;
+        Vector3 previousPos = pos + leftDir * viewDistance;
+        for (int i = 1; i <= segments; i++)
+        {
+            float angle = -viewAngle * 0.5f + (viewAngle / segments) * i;
+            Vector3 nextDir = Quaternion.AngleAxis(angle, Vector3.up) * transform.forward;
+            Vector3 nextPos = pos + nextDir * viewDistance;
+            Gizmos.DrawLine(previousPos, nextPos);
+            previousPos = nextPos;
+        }
+    }
+}
