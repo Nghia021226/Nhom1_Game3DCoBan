f4946c1d9ab8be8f3c2f833e79f5bb310c6215f8
Hieu1904c <nguyeenshieeus@gmail.com>
2026-02-04 14:17:59 +0700

    quai
    hg

---
+++ Assets/UnityAsset/Asset12012026/Creep Horror Creature/Prefabs/ChaseAction.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Unity.Behavior;
+using UnityEngine;
+using UnityEngine.AI;
+using Action = Unity.Behavior.Action;
+using Unity.Properties;
+
+[Serializable, GeneratePropertyBag]
+[NodeDescription(name: "Chase Player Custom", story: "Chase [Target] within [StopDistance]", category: "Action/Navigation", id: "ChaseAction")]
+public partial class ChaseAction : Action
+{
+    [SerializeReference] public BlackboardVariable<GameObject> Target;
+    [SerializeReference] public BlackboardVariable<float> StopDistance = new BlackboardVariable<float>(2f);
+    [SerializeReference] public BlackboardVariable<float> Speed = new BlackboardVariable<float>(5f);
+
+    private NavMeshAgent agent;
+
+    protected override Status OnStart()
+    {
+        if (Target.Value == null) return Status.Failure;
+        agent = GameObject.GetComponent<NavMeshAgent>();
+        if (agent == null) return Status.Failure;
+
+        agent.speed = Speed.Value;
+        agent.stoppingDistance = StopDistance.Value;
+        agent.isStopped = false;
+
+        return Status.Running;
+    }
+
+    protected override Status OnUpdate()
+    {
+        if (Target.Value == null) return Status.Failure;
+
+        agent.SetDestination(Target.Value.transform.position);
+
+        if (!agent.pathPending && agent.remainingDistance <= agent.stoppingDistance)
+        {
+            return Status.Success; // Đã đến gần, dừng lại để đánh
+        }
+
+        return Status.Running;
+    }
+}
